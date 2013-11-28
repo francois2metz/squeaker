0dc462e79b1ac379872c07b6f390b76283e6d768
François de Metz <francois@2metz.fr>
2013-11-28 15:02:57 +0100

    Extract methods to authenticator method.
    Signed-off-by: François de Metz <francois@stormz.me>

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,17 +3,21 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new
   end
 
+  class Authenticator < Struct.new(:controller)
+    def authenticate(username)
+      user = User.find_by_username(username)
+      if user
+        controller.login_successful(user)
+      else
+        controller.login_failed(username)
+      end
+    end
+  end
+
   def create
     username = params[:user_session][:username]
 
-    user = authenticate(username)
-    if user
-      login_successful(user)
-    else
-      login_failed(username)
-    end
-
-    redirect_to root_path, status: 303
+    Authenticator.new(self).authenticate(username)
   end
 
   def destroy
@@ -21,17 +25,13 @@ class UserSessionsController < ApplicationController
     redirect_to root_path
   end
 
-  protected
-
-  def authenticate(username)
-    User.find_by_username(username)
-  end
-
   def login_failed(username)
     flash[:error] = "No user with username '#{username}'"
+    redirect_to root_path, status: 303
   end
 
   def login_successful(user)
     session[:logged_in_user_id] = user.id
+    redirect_to root_path, status: 303
   end
 end
